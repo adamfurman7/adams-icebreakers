ae7f09da5917fcc8424853b949206844a64b6a37
Adam Furman <adamfurman7@gmail.com>
2022-09-25 12:53:59 -0500

    Added fun fact generator.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const joke = document.querySelector('#joke');
 const jokeBtn = document.querySelector('#jokeBtn');
+const fact = document.querySelector('#fact');
+const factBtn = document.querySelector('#factBtn');
 
 // Connect to API to get joke.
 const getJoke = async () => {
@@ -17,5 +19,20 @@ const getJoke = async () => {
     // console.log('getJoke worked!');
 }
 
+const getFact = async () => {
+    try {
+        const response = await axios.get('https://api.api-ninjas.com/v1/facts?limit=1', {
+            headers: {
+                'X-Api-Key': 'kk2ROoBBl6nrrFF4htgxxg==xieKOXfvrEiqNJv2',
+            },
+        });
+        fact.textContent = response.data[0].fact + ".";
+        console.log(response.data[0].fact);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 // Listen for button click and call getJoke function on click.
 jokeBtn.addEventListener('click', getJoke);
+factBtn.addEventListener('click', getFact);
